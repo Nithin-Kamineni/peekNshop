12edb75f6f8855507cc1a0df758a61c4412ff957
Nithin <nithinkamineni1@gmail.com>
2022-02-21 22:51:35 -0500

    database authentication with gorm

--- server/src/Stores/places/main.go
+++ server/src/Stores/places/main.go
@@ -1,29 +1,78 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/url"
+
+	"github.com/gorilla/mux"
 )
 
+type result struct {
+	results []struct{ results_info } `json:"results"`
+	status  string                   `json:"status"`
+}
+
+type results_info struct {
+	address_component []address_component_info `json:"address_component"`
+	formated_address  string                   `json:"formated_address"`
+	geometry          []geometry_info          `json:"geometry"`
+	place_id          string                   `json:"place_id"`
+	types             []string                 `json:"types"`
+}
+
+type address_component_info struct {
+	long_name  string   `json:"long_name"`
+	short_name string   `json:"short_name"`
+	types      []string `json:"types"`
+}
+
+type geometry_info struct {
+	bounds        []bounds_info `json:"bounds"`
+	location      []cord_info   `json:"location"`
+	location_type string        `json:"location_type"`
+	viewport      []bounds_info `json:"viewport"`
+}
+
+type bounds_info struct {
+	northeast []cord_info `json:"northeast"`
+	southwest []cord_info `json:"southwest"`
+}
+
+type cord_info struct {
+	lat float64 `json:"lat"`
+	lng float64 `json:"lng"`
+}
+
 func main() {
+	handleRequests()
+}
 
-	name := "John Doe"
-	occupation := "gardener"
+func handleRequests() {
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/address", returnLat)
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
 
-	params := "name=" + url.QueryEscape(name) + "&" +
-		"occupation=" + url.QueryEscape(occupation)
-	path := fmt.Sprintf("https://httpbin.org/get?%s", params)
+func returnLat(w http.ResponseWriter, r *http.Request) {
 
+	w.Header().Set("Content-Type", "application/json")
+
+	address := "1600+Amphitheatre+Parkway,+Mountain+View,+CA"
+	Key := "AIzaSyD02WdNCJWC82GGZJ_4rkSKAmQetLJSbDk"
+
+	params := "address=" + url.QueryEscape(address) + "&" +
+		"key=" + url.QueryEscape(Key)
+	path := fmt.Sprint("https://maps.googleapis.com/maps/api/geocode/json?", params)
+	fmt.Println(path)
 	resp, err := http.Get(path)
 
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 
@@ -31,5 +80,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	fmt.Println(string(body))
+	//data1 := result{}
+	var f interface{}
+	json.Unmarshal(body, &f)
+	fmt.Println(f)
+
+	json.NewEncoder(w).Encode(f)
+	defer resp.Body.Close()
 }
--- server/src/Users/main.go
+++ server/src/Users/main.go
@@ -2,51 +2,113 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
 
+	"github.com/google/uuid"
+	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
-type Stores struct {
-	StoresID  string `json:"StoresId"`
-	StoreName string `json:"StoreName"`
-	Catogiry  string `json:"catogiry"`
-}
-
-var stores []Stores
-
-//sending search results
-func getSearchResults(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(stores)
-}
-
-//getting nearest stores avilable based on the location of the user
-func getNearestStores(w http.ResponseWriter, r *http.Request) {
-
-}
-
-//sending inventory avilable in the store
-func getStoresDetails(w http.ResponseWriter, r *http.Request) {
-
-}
-
-//sending the details of the product
-func getProductDetails(w http.ResponseWriter, r *http.Request) {
-
-}
-
 func main() {
-	// Init Router
-	r := mux.NewRouter()
+	db, err := gorm.Open(sqlite.Open("../Users.db"), &gorm.Config{})
 
-	stores = append(stores, Stores{StoresID: "1", StoreName: "IndiMart", Catogiry: "Glosseries"})
-	//Rought handeler or endpoints
-	r.HandleFunc("/user/{id}/searchResults/{searchItem}", getSearchResults).Methods("POST")
-	r.HandleFunc("/user/{id}/stores", getNearestStores).Methods("GET")
-	r.HandleFunc("/user/{id}/stores/{storeId}", getStoresDetails).Methods("GET")
-	r.HandleFunc("/user/{id}/stores/{storeId}/{productId}", getProductDetails).Methods("GET")
+	if err != nil {
+		panic(err.Error())
+	}
 
-	log.Fatal(http.ListenAndServe(":8000", r))
+	app := App{
+		db: db,
+		r:  mux.NewRouter(),
+	}
+
+	app.start()
+}
+
+type App struct {
+	db *gorm.DB
+	r  *mux.Router
+}
+
+type user3 struct {
+	ID        string `gorm:"primary_key" json:"id"`
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+	Password  string `json:"password"`
+}
+
+func CORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+		// Set headers
+		w.Header().Set("Access-Control-Allow-Headers:", "*")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
+
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		fmt.Println("ok")
+
+		// Next
+		next.ServeHTTP(w, r)
+		return
+	})
+}
+
+func (a *App) start() {
+	a.db.AutoMigrate(&user3{})
+	a.r.HandleFunc("/user/", a.getAllUsers).Methods("GET")
+	a.r.HandleFunc("/user/", a.userSignUp).Methods("POST")
+	//a.r.HandleFunc("/students/{id}", a.updateStudent).Methods("PUT")
+	//a.r.HandleFunc("/students/{id}", a.deleteStudent).Methods("DELETE")
+	http.Handle("/", a.r)
+	log.Fatal(http.ListenAndServe(":10000", handlers.CORS(handlers.AllowedHeaders([]string{"X-Requested-With", "Content-Type", "Authorization"}), handlers.AllowedMethods([]string{"GET", "POST", "PUT", "HEAD", "OPTIONS"}), handlers.AllowedOrigins([]string{"*"}))(a.r)))
+	// log.Fatal(http.ListenAndServe(":10000", a.r))
+}
+
+func (a *App) getAllUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var all []user3
+	err := a.db.Find(&all).Error
+	if err != nil {
+		sendErr(w, http.StatusInternalServerError, err.Error())
+		return
+	}
+	err = json.NewEncoder(w).Encode(all)
+	if err != nil {
+		sendErr(w, http.StatusInternalServerError, err.Error())
+	}
+}
+
+// func sendErr(w http.ResponseWriter, i int, s string) {
+// 	panic("unimplemented")
+// }
+
+func sendErr(w http.ResponseWriter, code int, message string) {
+	resp, _ := json.Marshal(map[string]string{"error": message})
+	http.Error(w, string(resp), code)
+}
+
+func (a *App) userSignUp(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var s user3
+	err := json.NewDecoder(r.Body).Decode(&s)
+	if err != nil {
+		sendErr(w, http.StatusBadRequest, err.Error())
+		return
+	}
+	s.ID = uuid.New().String()
+	err = a.db.Save(&s).Error
+	if err != nil {
+		sendErr(w, http.StatusInternalServerError, err.Error())
+	} else {
+		w.WriteHeader(http.StatusCreated)
+	}
 }
