5bfa08e5c03aea8def247e9ecc75555d256591bf
vamsiraju835@gmail.com <vamsiraju835@gmail.com>
2022-03-19 16:46:20 -0400

    Receiving stores data while sending location of the user to the backend

--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit} from '@angular/core';
 import {MapsService} from '../services/maps.service';
-
+import { environment } from '../environments/environments'
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http"; 
+import { Stores } from '../models/common_models'
 
 @Component({
   selector: 'app-homepage',
@@ -12,10 +14,16 @@ export class HomepageComponent implements OnInit{
   subtitle = 'choose where to shop?';
   
 
-  constructor(public service: MapsService) { }
+  constructor(public service: MapsService, private http: HttpClient) { }
 
   ngOnInit(): void {
     this.service.getLocation();
+    setTimeout(() => {  this.http.get<Stores>('http://localhost:10000/address/?'+'search=store'+'&lat='+ environment.lat+'&long='+environment.lon, {}).subscribe( (data: Stores) => {
+      console.log(JSON.stringify(data))
+      console.log(data.icon)
+    }); }, 5000);
+    
+
   }
 
   
