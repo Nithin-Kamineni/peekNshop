1b667915389a811313b3eb9961f310c96e5e711f
vamsiraju835@gmail.com <vamsiraju835@gmail.com>
2022-04-06 21:32:04 -0400

    Made some minor changes

--- client/src/app/footer/footer.component.ts
+++ client/src/app/footer/footer.component.ts
@@ -1,12 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-
+import { environment } from '../environments/environments'
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
 export class FooterComponent implements OnInit {
-
+isLogin=environment.isLogin
   constructor() { }
 
   ngOnInit(): void {
