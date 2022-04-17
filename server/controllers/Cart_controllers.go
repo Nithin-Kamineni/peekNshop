3b3c058f206578af56b1c93f87afb0247f477c32
Nithin <nithinkamineni1@gmail.com>
2022-04-17 16:10:58 -0400

    clearing cart if user wants to clear the cart

--- server/controllers/Cart_controllers.go
+++ server/controllers/Cart_controllers.go
@@ -79,7 +79,6 @@ func CartManipulation(w http.ResponseWriter, r *http.Request) {
 func ClearCart(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	var cart models.Cart_items_db
-	var quantity int64
 
 	err := json.NewDecoder(r.Body).Decode(&cart)
 	if err != nil {
@@ -87,24 +86,10 @@ func ClearCart(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	err = utils.DB.Raw("SELECT quantity FROM store_inventories WHERE product_id = ?", cart.ProductID).Scan(&quantity).Error
+	err = utils.DB.Exec("DELETE cart_items_dbs where userID = ?", cart.UserID).Error
 	if err != nil {
 		sendErr(w, http.StatusInternalServerError, err.Error())
 		return
-	}
-	quantityInt, _ := strconv.ParseInt(cart.Quantity, 10, 0)
-	if quantity >= quantityInt {
-		err = utils.DB.Exec("UPDATE cart_items_dbs SET quantity = ?, ModifiedAt = ? where userID = ? and productID = ?", cart.Quantity, cart.ModifiedAt, cart.UserID, cart.ProductID).Error
-		if err != nil {
-			sendErr(w, http.StatusInternalServerError, err.Error())
-			return
-		}
-	} else {
-		reply := models.SignInReply{Msg: "Not enough inv to add items in cart"}
-		err = json.NewEncoder(w).Encode(reply)
-		if err != nil {
-			sendErr(w, http.StatusInternalServerError, err.Error())
-		}
 	}
 }
 
