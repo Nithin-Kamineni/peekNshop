46b98a6a4c678674fef8e70bcf5e8c8fc485211a
Sai Reddy <sai6221@gmail.com>
2022-04-20 03:31:06 -0400

    Added files for testing go API's

---
+++ server/controllers/User_controllers_test.go
@@ -0,0 +1,59 @@
+package controllers
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"src/models"
+	"src/utils"
+	"testing"
+)
+
+func checkResponseCode(t *testing.T, expected, actual int) {
+	if expected != actual {
+		t.Errorf("Expected response code %d. Got %d\n", expected, actual)
+	}
+}
+
+func Test_Login(t *testing.T) {
+	utils.ConnectTestDatabase()
+
+	req, err := http.NewRequest("GET", "/user?firstname=Sai&lastname=Reddy&email=nitin1@gmail.com&passkey=sai", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(UserLogin)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code : got %v want %v\n", status, http.StatusOK)
+	}
+
+}
+
+func Test_Signup(t *testing.T) {
+	utils.ConnectTestDatabase()
+
+	samplePerson := models.User3{
+		Firstname: "Sai",
+		Lastname:  "Reddy",
+		Email:     "nitin1@gmail.com",
+		Password:  "sai",
+	}
+	bytePerson, _ := json.Marshal(samplePerson)
+
+	req, err := http.NewRequest("POST", "/user", bytes.NewReader(bytePerson))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(UserSignUp)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code : got %v want %v\n", status, http.StatusOK)
+	}
+
+}
