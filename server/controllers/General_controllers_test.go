9f636700522fa0948dec2674a95833ecf64d2a08
Sai Reddy <sai6221@gmail.com>
2022-04-20 21:53:51 -0400

    Backend testing for General functions of the project

--- server/controllers/General_controllers_test.go
+++ server/controllers/General_controllers_test.go
@@ -1,14 +1,62 @@
 package controllers
 
-// import (
-// 	"bytes"
-// 	"encoding/json"
-// 	"net/http"
-// 	"net/http/httptest"
-// 	"src/models"
-// 	"src/utils"
-// 	"testing"
-// )
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"src/models"
+	"src/utils"
+	"testing"
+)
+
+func Test_HomePageReload(t *testing.T) {
+	utils.ConnectTestDatabase()
+
+	samplePerson := models.Coardinates{
+		Lat: "29.6213925",
+		Lon: "-82.37358560000001",
+	}
+	bytePerson, _ := json.Marshal(samplePerson)
+
+	req, err := http.NewRequest("POST", "/address/city", bytes.NewReader(bytePerson))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(HomePageReload)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code : got %v want %v\n", status, http.StatusOK)
+	}
+
+}
+
+func Test_ConvAddressToCord(t *testing.T) {
+	utils.ConnectTestDatabase()
+
+	samplePerson := models.AddressForm{
+		Street:  "3800 SW 34th",
+		City:    "Gainesville",
+		State:   "FL",
+		Zipcode: "32608",
+	}
+	bytePerson, _ := json.Marshal(samplePerson)
+
+	req, err := http.NewRequest("POST", "/address", bytes.NewReader(bytePerson))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+	handler := http.HandlerFunc(ConvAddressToCord)
+	handler.ServeHTTP(rr, req)
+	if status := rr.Code; status != http.StatusOK {
+		t.Errorf("handler returned wrong status code : got %v want %v\n", status, http.StatusOK)
+	}
+
+}
 
 // func Test_Login(t *testing.T) {
 // 	utils.ConnectTestDatabase()
